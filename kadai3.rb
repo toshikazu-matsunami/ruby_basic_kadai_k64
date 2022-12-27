80493396684a1890a09973fbc7bc61d125d5d114
LeHa2505 <leha25052001@gmail.com>
2022-12-27 13:01:13 +0700

    kadai 1 2 3 4

---
+++ kadai3.rb
@@ -0,0 +1,22 @@
+print "数字を入力してください > "
+input = gets.chomp
+
+begin
+  num = Integer(input)
+rescue ArgumentError
+  puts "整数ではありません"
+end
+
+if num
+  if num % 2 == 0 && num % 7 == 0
+    puts "#{num}は14の倍数です"
+  elsif num % 2 == 0
+    puts "#{num}は2の倍数です"
+  elsif num % 7 == 0
+    puts "#{num}は7の倍数です"
+  elsif num % 2 != 0 && num % 7 != 0
+    puts "#{num}は2の倍数でも7の倍数でもありません"
+  else
+    puts "整数ではありません"
+  end
+end
