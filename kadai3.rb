b1abcebb25b7d00917e54b1e6b4831559532b333
Replit user <>
2022-12-27 08:32:32 +0000

    done kadai3

---
+++ kadai3.rb
@@ -0,0 +1,16 @@
+print "数字を入力してください > "
+data_input = gets
+
+data_input = Integer data_input rescue nil;
+
+if data_input == nil
+    puts "整数ではありません"
+elsif data_input%7 == 0 && data_input %2 != 0
+    puts "7の倍数です"
+elsif data_input%7 != 0 && data_input %2 == 0
+    puts "2の倍数です"
+elsif data_input%7 == 0 && data_input %2 == 0
+    puts "14の倍数です"
+else
+    puts "2䛾倍数でも7䛾倍数でもありません"
+end
