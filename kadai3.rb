c9d8b5dbfcd076c9891e25a2d1bf00b0bf75c01a
Replit user <>
2022-12-27 05:31:15 +0000

    kadai3

---
+++ kadai3.rb
@@ -0,0 +1,15 @@
+puts '数字を入力してください >'
+input = gets
+input = input.chomp
+num = input.to_i
+if num.to_s != input
+  puts '整数ではありません'
+elsif num % 14 == 0
+  puts '14の倍数です'
+elsif num % 2 == 0
+  puts '2の倍数です'
+elsif num % 7 == 0
+  puts '7の倍数です'
+else
+  puts '2䛾倍数でも7䛾倍数でもありません'
+end
