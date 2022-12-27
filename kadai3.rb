3d637daa54f5fed16f5b5c89905ce0d767131b33
TrangNgan248 <trangnganvirgo@gmail.com>
2022-12-27 12:49:54 +0700

    kadai3

--- kadai3.rb
+++ kadai3.rb
@@ -1,5 +1,22 @@
-puts "数字を入力してください"
-number = gets.to_i
-if number.is_a? Interger && number % 2 != 0 && number % 7 != 0
-    puts K phải bội của 2 cũng k phải bội của 7
-elsif 
+print "数字を入力してください > "
+input = gets
+
+begin
+  num = Integer(input)
+rescue ArgumentError
+  puts "整数ではありません"
+end
+
+if num
+  if num % 2 == 0 && num % 7 == 0
+    puts "#{num}は14の倍数です"
+  elsif num % 2 == 0
+    puts "#{num}は2の倍数です"
+  elsif num % 7 == 0
+    puts "#{num}は7の倍数です"
+  elsif num % 2 != 0 && num % 7 != 0
+    puts "#{num}は2の倍数でも7の倍数でもありません"
+  else
+    puts "整数ではありません"
+  end
+end
