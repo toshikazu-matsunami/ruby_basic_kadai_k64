92900cc13184aa896b1e87aebf04269bec83e66f
ashley0147 <ashley.nguyen@avepoint.com>
2022-12-27 09:31:09 +0000

    edit ex3

--- kadai3.rb
+++ kadai3.rb
@@ -1,15 +1,18 @@
-puts '数字を入力してください'
-a = gets.to_i
-if a.even?
-  if a % 7 == 0
-    puts '14の倍数です'
-  else
-    puts '2の倍数です'
-  end
+puts '数字を入力してください > '
+number = gets.chomp
+if number != 0 && number.to_i.to_s != number.strip
+  puts '整数ではありません'
 else
-  if a % 7 == 0
+  number = number.to_i
+  if number.odd? && number % 7 != 0
+    puts '2の倍数でも7の倍数でもありません'
+  elsif number % 7 == 0 && number.even?
+    puts '14の倍数です'
+  elsif number.even?
+    puts '2の倍数です'
+  elsif number % 7 == 0
     puts '7の倍数です'
   else
-    puts '2の倍数でも7の倍数ではありません'
+    puts '整数ではありません'
   end
 end
