f5486be098b952b2ff20d58cbedc466da350e645
kawasemi2k1 <bovainghiengnang2k1@gmail.com>
2022-12-27 13:42:32 +0700

    kadai1234

--- kadai5.rb
+++ kadai5.rb
@@ -1,17 +1,21 @@
 print "学生数を代入してください > "
 n = gets.to_i   #ーザーが値を入れる
 i = 1           #宣言、０を代入
-arr = []        #配列を宣言
+meibo = []        #配列を宣言
 while i <= n
   puts "#{i}番目の学生の名前は何ですか？"
   print "名前> "
-  arr.push(gets)
+  name = gets
+  puts "#{i}番目の学籍番号は何ですか？"
+  print "学籍番号 >"
+  mssv = gets
+  meibo.push({"学籍番号" : mssv, "名前": name})
   i +=1
 end
 i = 1
 puts "-------------名簿---------------"
 while i <= n
-  puts "#{i}番目の学生は#{arr[i-1].chomp}です"
+  puts "#{i}番目の学生は#{meibo[i-1].chomp}です"
   i +=1
 end
 puts "--------------------------------"
