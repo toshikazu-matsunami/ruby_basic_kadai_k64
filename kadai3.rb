4826278501c0f9ccdaccfac059026437dab42e01
Replit user <>
2022-12-27 09:34:46 +0000

    update kadai3

--- kadai3.rb
+++ kadai3.rb
@@ -1,11 +1,16 @@
 print "数字を入力してください > "
-num = gets.to_i
-if num == 0
+input = gets
+begin 
+  num = Integer(input)
+  if num % 14 == 0 # 条件①
+    puts "14の倍数です"
+  elsif num % 2 == 0 # 条件②
+    puts "2の倍数です"
+  elsif num % 7 == 0 # 条件③ 
+    puts "7の倍数です"
+  else 
+    puts "14,2,7の倍数ではありません"
+    end
+rescue Exception
   puts "整数ではありません"
-elsif num % 14 == 0 # 条件①
-  puts "14の倍数です"
-elsif num % 2 == 0 # 条件②
-  puts "2の倍数です"
-elsif num % 7 == 0 # 条件③ 
-  puts "7の倍数です"
 end
