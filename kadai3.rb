1eac791f48cae0bfdfcdce4363a5ae5a389db49b
volmicrolab <volmicrolab@gmail.com>
2022-12-27 11:34:47 +0700

    kadai2

---
+++ kadai3.rb
@@ -0,0 +1,18 @@
+puts "NUMBERで代入してください"
+number = gets
+
+number = number.to_i
+
+puts("代入した: #{number}")
+
+if (number%7 == 0)
+    if ((number%2 == 0))
+        puts("14の倍数です")
+    else 
+        puts("7の倍数です")
+    end
+elsif (number%2 ==0) 
+   puts("2の倍数です")
+else 
+    puts("2䛾倍数でも7䛾倍数でもありません")
+end
