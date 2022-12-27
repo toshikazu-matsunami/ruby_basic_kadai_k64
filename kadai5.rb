b40429366fa7a28806a3de99ceca6d12c7af894f
LeHa2505 <leha25052001@gmail.com>
2022-12-27 14:08:28 +0700

    kadai5

--- kadai5.rb
+++ kadai5.rb
@@ -1,17 +1,21 @@
-print "学生数を設定してください > "
-num = gets.chomp.to_i
+print "学生䛾人数を入力してください > "
+num = gets.chomp.to_i;
 
-students = []
+student = []
 
-num.times do |i|
-  print "#{i + 1}番目䛾学生の名前は何ですか?\n"
+  num.times do |i|
+  puts "#{i+1}番目䛾学生䛾名前䛿何ですか?"
   print "名前 > "
   name = gets.chomp
-  students << name
-end
+  puts "#{i+1}番目䛾学籍番号䛿何ですか?"
+  print "学籍番号 > "
+  mssv = gets.chomp
+  student.push({"学籍番号": mssv, "名前": name})
+  end
+  
+  puts "-------------名簿---------------"
+  student.each do |i|
+  puts "学籍番号 #{i[:"学籍番号"]} - 名前 #{i[:"名前"]}"
+  end
+  puts "-------------------------------"
 
-print "-------------名簿---------------\n"
-students.each_with_index do |student, index|
-    puts "+ #{index + 1}番目の学生は#{student}です\n"
-end
-print "-------------------------------\n"
