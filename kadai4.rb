7365054987f8e6e5506a20b65334974cad4673f4
volmicrolab <volmicrolab@gmail.com>
2022-12-27 13:39:53 +0700

    kadai 12345

--- kadai4.rb
+++ kadai4.rb
@@ -2,7 +2,7 @@ puts "学生䛾人数を入力してください"
 number = gets.to_i
 storage = [];
 for index in 0..number-1 do
-    puts("#{index+1}番目䛾学生䛾名前は何ですか?")
+    puts("#{index+1}番目は学生の名前は何ですか?")
     name= gets.chomp
     storage.push(name);
 end
@@ -10,5 +10,5 @@ i=0;
 storage.each {
     |name|
     i=i+1;
-    puts("#{}番目䛾学生は#{name}です")
+    puts("#{i}番目䛾学生は#{name}です")
 }
