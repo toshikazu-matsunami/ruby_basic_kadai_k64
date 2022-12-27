1a71ceee5b1b63cbf8104d12556f9ebe3ceb05f5
Replit user <>
2022-12-27 09:07:32 +0000

    kadai1234

--- kadai3.rb
+++ kadai3.rb
@@ -1,14 +1,26 @@
 puts "numberで代入してください"
-number = gets.chomp.to_i
+number = gets
+
+def letter?(lookAhead)
+    lookAhead.match?(/[[:alpha:]]/)
+end
+  
+def numeric?(lookAhead)
+    lookAhead.match?(/[[:digit:]]/)
+end
+
 # check number or character
-if number.is_a?(Integer) == false
+if letter?(number) || number % 1 != 0
     puts "整数ではありません"
-elsif number % 2 == 0 && number % 7 != 0
-    puts "2の倍数です"
-elsif number % 7 == 0 && number % 2 != 0
-    puts "7の倍数です"
-elsif number % 2 == 0 && number % 7 == 0
-    puts "14の倍数です"
 else
-    puts "2の倍数でも7の倍数でもありません"
+    
+    if number.to_i % 2 == 0 && number.to_i % 7 != 0
+        puts "2の倍数です"
+    elsif number.to_i % 7 == 0 && number.to_i % 2 != 0
+        puts "7の倍数です"
+    elsif number.to_i % 2 == 0 && number.to_i % 7 == 0
+        puts "14の倍数です"
+    else
+        puts "2の倍数でも7の倍数でもありません"
+    end
 end
