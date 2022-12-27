5689833b786198da2368c0f04a021262391f033d
Replit user <>
2022-12-27 09:04:48 +0000

    kadai1234

---
+++ kadai3.rb
@@ -0,0 +1,20 @@
+puts "数字を入力してください"
+
+number = gets
+
+begin
+	Integer(number)
+rescue => e
+	puts "整数ではありません"
+else
+	number = number.to_i
+	if number%2==0 && number%7==0
+		puts "14の倍数です"
+	elsif number%2==0
+		puts "2の倍数です"
+	elsif number%7==0
+		puts "7の倍数です"
+	else
+		puts "2の倍数でも7の倍数でもありません"
+	end
+end	
