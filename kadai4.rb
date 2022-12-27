598e75d459ea7f1117333fd584a4a206f6a85029
TrangNgan248 <trangnganvirgo@gmail.com>
2022-12-27 12:52:56 +0700

    kadai4

--- kadai4.rb
+++ kadai4.rb
@@ -3,8 +3,8 @@ num = gets.to_i
 
 students = []
 
-num.times do
-  print "1番目䛾学生䛾名前䛿何ですか?\n"
+num.times do |i|
+  print "#{i+1}番目の学生の名前は何ですか?\n"
   print "名前 > "
   name = gets.chomp
   students << name
@@ -12,6 +12,6 @@ end
 
 print "-------------名簿---------------\n"
 students.each_with_index do |student, index|
-    puts "+ #{index + 1}番目䛾学生䛿#{student}です\n"
+    puts "+ #{index + 1}番目の学生は#{student}です\n"
 end
 print "-------------------------------\n"
