696cefa27e554c8ee11084e75566782735f8148a
Replit user <>
2022-12-29 09:38:39 +0000

    kadai3 incomplete, kadai4 complete

--- kadai3.rb
+++ kadai3.rb
@@ -1,5 +1,6 @@
 print "数字を入力してください > "
-number = gets.to_i
+number = gets.chomp
+pattern = /^[+-]?[1-9]([0-9]*)?$/
 
 =begin
 if number.is_a? Integer
@@ -9,14 +10,17 @@ else
 end
 =end
 
-if (number % 2 == 0) && (number % 7 == 0)
-  puts "14の倍数です"
-elsif number % 2 == 0
-  puts "2の倍数です"
-elsif number % 7 == 0
-  puts "7の倍数です"
-elsif number % 1 == 0
-  puts "2の倍数でも7の倍数でもありません"
-else
-  puts "整数ではありません"
+if number == pattern
+  x = x.to_i
+  if (number % 2 == 0) && (number % 7 == 0)
+    puts "14の倍数です"
+  elsif number % 2 == 0
+    puts "2の倍数です"
+  elsif number % 7 == 0
+    puts "7の倍数です"
+  elsif number % 1 == 0
+    puts "2の倍数でも7の倍数でもありません"
+  else
+    puts "整数ではありません"
+  end
 end
--- kadai4.rb
+++ kadai4.rb
@@ -1,14 +1,17 @@
 print "学生の人数を入力してください > "
 number_of_students = gets.to_i
 count = 0
-
-
-students = [
-]
+students = []
 
 [*1..number_of_students].each do |count|
   puts "#{count}番目の学生の名前は何ですか？"
   print "名前 > "
-  student_name = gets
+  student_name = gets.chomp
   students << {number: count, name: student_name}
 end
+
+puts "----------名簿----------"
+students.each do |student|
+  puts "+ #{student[:number]}番目の学生は#{student[:name]}です。"
+end
+puts "------------------------"
