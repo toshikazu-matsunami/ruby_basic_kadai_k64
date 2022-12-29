6996f2ee743d800ab651ba9b8c1221a21054c29f
phuongctt1 <111304159+phuongctt1@users.noreply.github.com>
2022-12-29 15:45:12 +0700

    kadai4
    kadai4

--- main.rb
+++ main.rb
@@ -1,14 +1,16 @@
-puts '好きな年を西暦（例：2000）で代入してください'
-year = gets.to_i
-# 以下の条件分岐に、新しい条件を追加してください
-if year < 1986
-  puts "#{year}年にオリンピックはまだありません"
-elsif (year == 1916) || (year == 1940) || (year == 1944) || (year == 2020)
-  puts "#{year}年は夏季オリンピックが開催されませんました"
-elsif year == 2021
-  puts '「コロナ䛾せいで1年遅れで2021年に開催されました」'
-elsif year % 4 == 0
-  puts "#{year}年は夏季オリンピックが開催されました"
-else
-  puts "#{year}年は夏季オリンピックイヤーではありません"
+print '学生䛾人数を入力してください >'
+num = gets.chomp.to_i
+
+list_student = []
+
+(1..num).each do |i|
+  puts "#{i}番目の学生の名前は何ですか?"
+  print '名前 > '
+  name = gets.chomp
+  list_student << name
 end
+puts '--------------名簿----------------'
+list_student.each_with_index do |student, index|
+  puts "+ #{index + 1}番目䛾学生䛿#{student}です"
+end
+puts '---------------------------------'
