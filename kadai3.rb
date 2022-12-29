c8151ee695c45d09c76c89490df9968ac517ec18
phuongctt1 <111304159+phuongctt1@users.noreply.github.com>
2022-12-29 16:10:27 +0700

    Update kadai3.rb

--- kadai3.rb
+++ kadai3.rb
@@ -1,13 +1,13 @@
 print '数字を入力してください >'
-val = gets.chomp.to_i
-if val != 0
-  if val.even?
-    if val % 7 == 0
+tmp = gets.chomp.to_i
+if tmp != 0
+  if tmp.even?
+    if tmp % 7 == 0
       puts '14の倍です'
     else
       puts '2の倍数です'
     end
-  elsif val % 7 == 0
+  elsif tmp % 7 == 0
     puts '7の倍数です'
   else
     puts '2䛾倍数でも7䛾倍数でもありません'
