77122eb47d4c28cbc8d092d3bbf1bb2f1c6da1ac
“NguyenTungSon0503” <sonmeliodas0503@gmail.com>
2022-12-29 15:26:48 +0700

    update_kadai3

--- kadai3.rb
+++ kadai3.rb
@@ -1,21 +1,17 @@
-puts '好きな年を代入してください'
+puts '好きな数値を代入してください'
 num = gets.chomp
 
 if (num.include? ".")== false
   num =num.to_i
-  if num%2 == 0
-    if num % 7 == 0
+  if num%2 == 0 && num%7 == 0
       puts '14の倍数です'
-    else
+  elsif num%2 == 0 && num%7 != 0
       puts '2の倍数です'
-    end
-  else
-    if num % 7 == 0
+  elsif num%2 != 0 && num%7 == 0
       puts '7の倍数です'
-    else
-      puts '2䛾倍数でも7䛾倍数でもありません'
+  else
+      puts '2の倍数でも7の倍数でもありません'
     end
-  end
 else
   puts '整数ではありません'
 end
