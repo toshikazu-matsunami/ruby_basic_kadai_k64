817e6e8d88594b9a7b50d455d36c7adc8eb4f291
huylinh <nguyenhuylinhvt98@gmail.com>
2023-01-01 23:43:21 +0700

    kadai5.rb

--- kadai5.rb
+++ kadai5.rb
@@ -1,17 +1,17 @@
 print "学生の人数を入力してください＞"
 number = gets.to_i
 i = 0
-students = Array.new
+students = Hash.new
 while i < number
     puts "#{i+1}番目の学生の名前は何ですか?"
+    print "名前＞"
     name = gets.chomp
+    print "学籍番号＞"
+    idNumber= gets.chomp
+    students.store(idNumber,name)
     i+=1
 end
 
-i=0
 puts "-------------名簿---------------"
-while i < number
-    puts "+ #{i+1}番目の学生は#{students.at(i)}です"
-    i+=1
-end
+students.each_pair {|key, value| puts "学籍番号#{key} - 名前#{value}"} 
 puts "-------------------------------"
