e76ecbe62c78f16e7fe93d74fc7b1dcdbb64afb1
Replit user <>
2023-01-01 22:51:43 +0000

    kadai5

--- kadai3.rb
+++ kadai3.rb
@@ -1,22 +1,21 @@
-print '数字を入力してください >'
+print "数字を入力してください >"
 
 val = gets.chomp.to_i
-
+p val
 if val != 0
-  if val.even?
-    if val % 7 == 0
-      puts '14の倍です'
+  if val%2==0
+    if val%7==0
+      puts "14の倍です"
     else
-      puts '2の倍数です'
+      puts "2の倍数です"
     end
-  else
-    if val % 7 != 0
-      puts '2の倍数でも7の倍数でもありません'
+  else 
+    if val%7!=0 
+      puts "2の倍数でも7の倍数でもありません"
     else
-      puts '7の倍数です'
+      puts "7の倍数です"
     end
   end
 else
-  puts '整数ではありません'
+  puts "整数ではありません"
 end
-# end
