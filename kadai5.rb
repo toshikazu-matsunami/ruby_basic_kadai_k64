e40d5bde7849ba3d6b94b83b8f26fe8d54f7ded7
tungnguyensnk <tung.nd194714@sis.hust.edu.vn>
2023-01-02 11:43:25 +0700

    kadai5

--- kadai5.rb
+++ kadai5.rb
@@ -1,20 +1,19 @@
-print "学生数を代入してください >"
-n = gets.to_i
-i = 1
-meibo = []
-while i <= n
-  puts "#{i}番目の学生の名前は何ですか？"
-  print "名前>"
+print '学生䛾人数を入力してください > '
+numStudent = gets.chomp.to_i
+list_students = []
+
+(0..numStudent - 1).each do |i|
+  puts "#{i + 1}番目䛾学生䛾名前䛿何ですか?"
+  print("名前 > ")
   name = gets.chomp
-  puts "#{i}番目の学籍番号は何ですか？"
-  print "学籍番号 >"
-  mssv = gets.chomp
-  meibo << { 学籍番号: mssv, 名前: name }
-  i += 1
+  puts "#{i + 1}番目の学籍番号は何ですか?"
+  print '学籍番号 > '
+  id = gets.chomp.to_i
+  list_students << {name: name, student_id: id}
 end
 
-puts "-------------名簿---------------"
-meibo.each do |gakusei|
-  puts "学籍番号 #{gakusei[:学籍番号]} - 名前 #{gakusei[:名前]}"
+puts('-------------名簿---------------')
+list_students.each do |student|
+  puts "学籍番号: #{student[:student_id]} - 名前: #{student[:name]}"
 end
-puts "--------------------------------"
+puts('-------------------------------')
