7d27920ee78cd8a741f5b306f9a36177273decc6
ChuogZug <90974849+ChuogZug@users.noreply.github.com>
2023-01-03 11:55:37 +0000

    Edit full

--- kadai4.rb
+++ kadai4.rb
@@ -1,3 +1,17 @@
-akwwk
-ppiw
-q
+num = gets.to_i
+if num.is_a ? Integer == true
+  puts "Not an integer"
+end
+students = []
+
+num.times do |i|
+  print "#{i + 1}番目の学生の名前は何ですか?\n"
+  print '名前 > '
+  name = gets.chomp
+  students << name
+end
+
+print "名簿: "
+students.each_with_index do |student, index|
+  puts "+ #{index + 1}番目の学生は#{student}です\n"
+end
