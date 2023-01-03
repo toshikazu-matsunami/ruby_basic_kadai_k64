227b6e36df579bd227fd327350de58f6f8db3f6c
hoapng <96011909+hoapng@users.noreply.github.com>
2023-01-03 20:44:05 +0700

    Update kadai5.rb

--- kadai5.rb
+++ kadai5.rb
@@ -1,21 +1,21 @@
-print '学生䛾人数を入力してください >'
-num = gets.to_i
+print'学生の人数を入力してください >'
+num=gets.to_i
 
-student = Array.new(num) {Hash.new}
+student=Array.new(num){Hash.new}
 (1..num).each do |i|
-  puts "#{i}番目䛾学生䛾名前䛿何ですか?"
-  print '名前 > '
-  name = gets.chomp.to_s
-  puts "#{i}番目䛾学籍番号䛿何ですか?"
-  print '学籍番号 > '
-  mssv = gets.chomp.to_s
-  student[i-1][:name] = name
-  student[i-1][:mssv] = mssv
+  puts"#{i}番目䛾学生䛾名前䛿何ですか?"
+  print'名前 >'
+  name=gets.chomp.to_s
+  puts"#{i}番目䛾学籍番号䛿何ですか?"
+  print'学籍番号 >'
+  mssv=gets.chomp.to_s
+  student[i-1][:name]=name
+  student[i-1][:mssv]=mssv
   
 end
 
-puts '-------------名簿---------------'
+puts'------------名簿--------------'
 (1..num).each do |i|
-  puts "学籍番号 #{student[i-1][:mssv]} - 名前 #{student[i-1][:name]}"
+  puts"学籍番号 #{student[i-1][:mssv]}-名前 #{student[i-1][:name]}"
 end
-puts '-------------------------------'
+puts'-----------------------------'
