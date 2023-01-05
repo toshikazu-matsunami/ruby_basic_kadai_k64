b7ddfdcacf2b717b9e4fc4de39a910262765f99c
QuachHoa <98033796+QVH201@users.noreply.github.com>
2023-01-05 08:08:56 +0000

    update 05/01

--- kadai3.rb
+++ kadai3.rb
@@ -1,6 +1,12 @@
 print "数字を入力してください > "
-number = gets.to_i
+input = gets
+begin 
+  number = Integer(input)
+rescue ArgumentError
+  puts "整数ではありません"
+end
 
+if number
 if number % 2 == 0 && number % 7 != 0
   puts "2の倍数です"
 end
@@ -13,7 +19,7 @@ end
 if number % 2 != 0 && number % 7 != 0 
   puts "2の倍数でも7の倍数でもありません"
 end
-if number % 2 !=0 && number % 2 != 1
-  puts "整数ではありません"
 end
+
+
 
